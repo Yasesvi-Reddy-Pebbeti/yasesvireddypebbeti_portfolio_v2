b9b393bafd06fe78d3fac5ef3354c047262f112d
Yasesvi Reddy Pebbeti <yasesvireddy426@gmail.com>
2026-04-15 20:29:55 -0400

    sitemap

--- src/app/Sitemap.js
+++ src/app/Sitemap.js
@@ -1,10 +1,10 @@
+// src/app/sitemap.js
+
 export default function sitemap() {
   return [
     {
       url: "https://yasesvireddy.dev",
       lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 1.0,
     },
   ];
 }
