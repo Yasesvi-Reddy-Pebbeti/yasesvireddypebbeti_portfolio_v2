ee73f662b0519e7f8694fff1866dda8988c25ef4
Yasesvi Reddy Pebbeti <yasesvireddy426@gmail.com>
2026-04-15 20:06:16 -0400

    add meta data

---
+++ src/app/Sitemap.js
@@ -0,0 +1,11 @@
+// app/sitemap.js
+export default function sitemap() {
+  return [
+    {
+      url: "https://yasesvireddy.dev/",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1.0,
+    },
+  ];
+}
